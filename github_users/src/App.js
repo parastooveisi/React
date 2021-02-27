1f8971b0bd8a07e3f450c0bd077c37b5d1ced30d
parastooveisi <parastoo.veisi@gmail.com>
2021-02-27 11:29:36 -0600

    Use useEffect to fetch data from GitHub and display the information as a component

--- github_users/src/App.js
+++ github_users/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const url = "https://api.github.com/users";
+  const [users, setUsers] = useState([]);
+  useEffect(async () => {
+    const result = await axios(url);
+    setUsers(result.data);
+  }, []);
+
+  const renderedUsers = users.map((user) => {
+    const { login, id, avatar_url, html_url } = user;
+    return (
+      <li key={id}>
+        <img src={avatar_url} alt={login} />
+        <div>
+          <h4> {login}</h4>
+          <a href={html_url}>profile</a>
+        </div>
+      </li>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 align="center">GitHub Users</h2>
+      <ul className="rows">{renderedUsers}</ul>
     </div>
   );
 }
