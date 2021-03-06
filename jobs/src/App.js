8f8f92f82dcfc879c6d70eed88af90b10386930e
parastooveisi <parastoo.veisi@gmail.com>
2021-03-06 13:24:09 -0600

    Create an array of data from faker.js

--- jobs/src/App.js
+++ jobs/src/App.js
@@ -3,10 +3,32 @@ import Jobs from "./Jobs";
 import faker from "faker";
 
 const App = () => {
-  console.log(faker.internet.email());
+  const generator = (schema, min = 1, max) => {
+    max = max || min;
+    return Array.from({ length: faker.random.number({ min, max }) }).map(() =>
+      Object.keys(schema).reduce((entity, key) => {
+        entity[key] = faker.fake(schema[key]);
+        return entity;
+      }, {})
+    );
+  };
+
+  // your schema
+  const clientsSchema = {
+    id: "{{random.number}}",
+    prductName: "{{commerce.productName}}",
+    description: "{{commerce.productDescription}}",
+    price: "{{commerce.price}}",
+    image: "{{image.business}}",
+  };
+
+  // generate random clients between 5 and 20 units, based on client schema defined above
+  const data = generator(clientsSchema, 5, 20);
+
+  console.log(data);
   return (
     <div>
-      <Jobs />
+      <Jobs data={data} />
     </div>
   );
 };
