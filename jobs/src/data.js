f02cd1b592237ed3c3bd881334a81c89d4514c2f
parastooveisi <parastoo.veisi@gmail.com>
2021-03-09 11:01:59 -0600

    Add data to the project

---
+++ jobs/src/data.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    id: 0,
+    name: "Amazon",
+    jobTitle: "Front End Engineer",
+    description:
+      "We are looking for strong front-end engineers who are results-oriented, data focused, passionate, and innovative in creating new, comprehensive customer engagement services and solutions. You’ll work on cutting edge technology such as such as React, Redux, Flow, Webpack, ES6+, and AWS to build the world’s most innovative customer experience solutions.",
+    salary: "6200",
+    imge:
+      "https://cdn.vox-cdn.com/thumbor/hiDV5Agcp5kqHfi-uZw5WEL_kQc=/0x0:2040x1360/1520x1013/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/66504186/acastro_181114_1777_amazon_hq2_0006.0.jpg",
+  },
+  {
+    id: 1,
+    name: "Facebook",
+    jobTitle: "Software Engineer",
+    description:
+      "Facebook is seeking AI Software Engineers to join our Research & Development teams. The ideal candidate will have industry experience working on a range of problems. The position will involve taking these skills and applying them to some of the most exciting and massive social data and prediction problems that exist on the web. We are hiring in multiple locations.",
+    salary: "5000",
+    imge:
+      "https://cdn.vox-cdn.com/thumbor/AzzLR7vYFu66e8hyJplAKFQlesg=/0x0:2040x1360/1520x1013/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/67134767/jbareham_180405_1777_facebook_0003.0.jpg",
+  },
+  {
+    id: 2,
+    name: "Aucerna",
+    jobTitle: "Software Developer",
+    description:
+      "Are you motivated by big ideas and driven by being part of an organization dedicated to helping our clients achieve the exceptional?We are transforming the way our clients make planning and execution decisions in the dynamic oil and gas industry. Our Calgary development team is looking for people who are inspired by challenge. The harder the problem, the more they dig in. We are building something special. Are you in?",
+    salary: "6500",
+    imge:
+      "https://prnewswire2-a.akamaihd.net/p/1893751/sp/189375100/thumbnail/entry_id/0_1rp6a8l2/def_height/725/def_width/1385/version/100012/type/2/q/100",
+  },
+  {
+    id: 3,
+    name: "Helcim",
+    jobTitle: "Junior Full Stack Developer",
+    description:
+      "Helcim is searching for a Junior Developer to be responsible for helping develop back-end and front-end systems. We're looking for a talented individual with a passion for coding and strong problem-solving skills.As a member of our development team, you will be working on numerous ongoing and new projects. Helcim is a fast-paced, high-demand company and team members must work well under pressure. This is a junior position; we train talented but less-experienced developers and help further their skill sets.",
+    salary: "4500",
+    imge: "https://www.helcim.com/images/posters/Helcim-Poster-Horizontal.jpg",
+  },
+  {
+    id: 4,
+    name: "Absorb Software",
+    jobTitle: "Junior Quality Assurance Analyst",
+    description:
+      "f you’re known for seeking out and soaking up information so that you can understand the “why” and find yourself signing up for tech talks and lunch and learns because you have a thirst for learning, our Junior Quality Assurance (QA) Analyst role might be right for you.Absorb’s Product team is looking for two Junior Quality Assurance Analysts at the start of their careers who have under 3 years of QA experience and passion for learning.There’s no shortage of variety in the QA day-to-day work so being motivated to prioritize testing and support is critical to your success in the role. Experienced in web application testing with an understanding of the SaaS working environment, you are a true multi-tasker, who is team focused and a quality advocate, with a good mix of knowledge and people skills.",
+    salary: "4500",
+    imge:
+      "https://prnewswire2-a.akamaihd.net/p/1893751/sp/189375100/thumbnail/entry_id/1_yw4pep4q/def_height/671/def_width/1559/version/100011/type/1",
+  },
+  {
+    id: 5,
+    name: "Adlib",
+    jobTitle: "Software Developer",
+    description:
+      "As the global leader in Advanced Rendering technology, Adlib helps organizations in the Energy, Life Sciences, Insurance, and Banking sectors, among others, enhance content-centric processes by unlocking the value in unstructured content.Adlib's headquarters are located in Burlington, Ontario. As a company, we care about employee health and wellness and develop programs that align to achieving personal and professional satisfaction. We are also passionate about our community and participate in many local charitable events.At Adlib we have a number of employee programs and benefits that drive our culture and values",
+
+    salary: "5500",
+    imge:
+      "https://d2q79iu7y748jz.cloudfront.net/s/_squarelogo/bfde758298f6066776c60cbd64bb249d",
+  },
+];
