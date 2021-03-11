f306c91d0e71f901ef9ea4432ff818843d418217
parastooveisi <parastoo.veisi@gmail.com>
2021-03-10 18:17:02 -0600

    Add data

---
+++ accordion/src/data.js
@@ -0,0 +1,34 @@
+const questions = [
+  {
+    id: 1,
+    title: "When will the vaccine be available to me?",
+    info:
+      "The goal is for everyone to be able easily to get a COVID-19 vaccination as soon as large quantities of vaccine are available. As the vaccine supply increases, more groups will be added to receive vaccination.",
+  },
+  {
+    id: 2,
+    title: "If I am pregnant, can I get a COVID-19 vaccine?",
+    info:
+      "Yes. If you are pregnant, you may choose to be vaccinated when it’s available to you. There is currently no evidence that antibodies formed from COVID-19 vaccination cause any problem with pregnancy, including the development of the placenta.",
+  },
+  {
+    id: 3,
+    title: "How long does the protection from a vaccine last?",
+    info:
+      "We don’t know how long protection lasts for those who are vaccinated. What we do know is that COVID-19 has caused very serious illness and death for a lot of people. If you get COVID-19, you also risk giving it to loved ones who may get very sick. Getting a COVID-19 vaccine is a safer choice.",
+  },
+  {
+    id: 4,
+    title: "Who is paying for the COVID-19 vaccines?",
+    info:
+      "The federal government is providing the vaccine free of charge to all people living in the United States. Vaccination providers can be reimbursed for vaccine administration fees by the patient’s public or private insurance company or, for uninsured patients, by the Health Resources and Services Administration’s Provider Relief Fund",
+  },
+  {
+    id: 5,
+    title:
+      "Can I get vaccinated against COVID-19 while I am currently sick with it?",
+    info:
+      "No. People with COVID-19 who have symptoms should wait to be vaccinated until they have recovered from their illness and have met the criteria for discontinuing isolation; those without symptoms should also wait until they meet the criteria before getting vaccinated",
+  },
+];
+export default questions;
